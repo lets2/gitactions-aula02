ee527d5a3c6d1c8da7ac6928d9aebf4d9a5d1943
Letonio <letoniojs@gmail.com>
2023-05-27 21:15:13 -0300

    upd workflow build to test too

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -8,68 +8,70 @@ router.use(Express.json());
 router.use(Express.urlencoded({ extended: true }));
 
 // ----- ROTAS ------
-router.get("/", (req: Request, res: Response) => {
-  res.sendFile(path.join(__dirname, "../../public", "index.html"));
+router.get("/", (req: path, res: Response) => {
+    res.sendFile(path.join(__dirname, "../../public", "index.html"));
 });
 
 router.get("/todo", (req: Request, res: Response) => {
-  try {
-    // SIMULANDO UMA CONSULTA NO BANCO DE DADOS
-    const resultado = tarefas;
+    try {
+        // SIMULANDO UMA CONSULTA NO BANCO DE DADOS
+        const resultado = tarefas;
 
-    // RESPOSTA COM OS DADOS
-    res.status(200).send(resultado);
-  } catch (error) {
-    res.status(400).send({ erro: error });
-  }
+        // RESPOSTA COM OS DADOS
+        res.status(200).send(resultado);
+    } catch (error) {
+        res.status(400).send({ erro: error });
+    }
 });
 
 router.post("/todo", (req: Request, res: Response) => {
-  try {
-    if (!req.body?.tarefa) {
-      throw "formato de requisição incorreto :(";
+    try {
+        if (!req.body?.tarefa) {
+            throw "formato de requisição incorreto :(";
+        }
+        const tarefa = req.body.tarefa;
+
+        // SIMULAÇÃO UMA INSERÇÃO NO BANCO DE DADOS
+        // novoId é 1 se o banco está vazio, ou 1 a mais que o ID da última tarefa do banco
+        const novoId = (tarefas[tarefas.length - 1]?.id || 0) + 1;
+        tarefas.push({ id: novoId, descricao: `${tarefa}` });
+
+        // RESPOSTA COM STATUS 200
+        res.status(200).send({
+            id: novoId,
+            mensagem: `Tarefa '${tarefa}' inserida com sucesso!`,
+        });
+    } catch (error) {
+        res.status(400).send({ erro: error });
     }
-    const tarefa = req.body.tarefa;
-
-    // SIMULAÇÃO UMA INSERÇÃO NO BANCO DE DADOS
-    // novoId é 1 se o banco está vazio, ou 1 a mais que o ID da última tarefa do banco
-    const novoId = (tarefas[tarefas.length - 1]?.id || 0) + 1;
-    tarefas.push({ id: novoId, descricao: `${tarefa}` });
-
-    // RESPOSTA COM STATUS 200
-    res.status(200).send({
-      id: novoId,
-      mensagem: `Tarefa '${tarefa}' inserida com sucesso!`,
-    });
-  } catch (error) {
-    res.status(400).send({ erro: error });
-  }
 });
 
 router.delete("/todo/:id", (req: Request, res: Response) => {
-  try {
-    if (!req.params?.id) {
-      throw "Requisição sem ID";
+    try {
+        if (!req.params?.id) {
+            throw "Requisição sem ID";
+        }
+        const idParaDeletar = Number(req.params.id);
+
+        // -1 se não encontrou
+        const index = tarefas.findIndex(
+            (tarefa) => tarefa.id === idParaDeletar
+        );
+
+        if (index === -1) {
+            res.status(404).send({ mensagem: `ID não encontrado!` });
+            return;
+        }
+
+        // deletar no banco (splice) e mandar status 200
+        const tarefa = tarefas[index];
+        tarefas.splice(index, 1);
+        res.status(200).send({
+            mensagem: `Tarefa '${tarefa.descricao}' deletada com sucesso!`,
+        });
+    } catch (error) {
+        res.status(400).send({ erro: error });
     }
-    const idParaDeletar = Number(req.params.id);
-
-    // -1 se não encontrou
-    const index = tarefas.findIndex((tarefa) => tarefa.id === idParaDeletar);
-
-    if (index === -1) {
-      res.status(404).send({ mensagem: `ID não encontrado!` });
-      return;
-    }
-
-    // deletar no banco (splice) e mandar status 200
-    const tarefa = tarefas[index];
-    tarefas.splice(index, 1);
-    res.status(200).send({
-      mensagem: `Tarefa '${tarefa.descricao}' deletada com sucesso!`,
-    });
-  } catch (error) {
-    res.status(400).send({ erro: error });
-  }
 });
 
 export { router };
